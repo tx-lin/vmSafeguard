17ff7ac2e577c82a61cc2817bb7154a0b63025f7
brlndtech <brlndtech@gmail.com>
2020-07-06 16:29:27 +0200

    show-log.php, and other little things

--- controller.php
+++ controller.php
@@ -2,4 +2,5 @@
 $PORT=22; // TO CHANGE 
 $HOST="10.69.255.41"; // TO CHANGE
 $CHACKBACKUPFOLDER="ls -dt /vmfs/volumes/5e566f71-06f2da78-82d5-441ea15ee924/backup*"; // TO CHANGE !
+$LOG="/vmfs/volumes/5e566f71-06f2da78-82d5-441ea15ee924/scripts/logbackup.txt";
 ?>
