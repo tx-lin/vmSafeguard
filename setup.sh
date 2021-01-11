0b7c87483e6b1fec3f67489a18ab73ca08e6459a
Brlndtech <38756128+brlndtech@users.noreply.github.com>
2021-01-11 09:56:03 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 echo -e "\e[33m The installation process of EWMT will start in few seconds ... \e[39m"
 sleep 1
 sudo apt -y install git apache2 php sqlite3 php-sqlite3 htop wget sudo curl
-sudo git clone https://github.com/brlndtech/ESXi-Web-Management-Tool
-sudo mv ESXi-Web-Management-Tool /var/www/html
-sudo chmod 700 -R /var/www/html/ESXi-Web-Management-Tool
-sudo chown -R www-data:www-data /var/www/html/ESXi-Web-Management-Tool # seulement l'utilisateur www:data et son grp aura des droits RWX, pour exec les scripts.php / ewmt.db
+sudo git clone https://github.com/brlndtech/vmSafeguard
+sudo mv vmSafeguard /var/www/html
+sudo chmod 700 -R /var/www/html/vmSafeguard
+sudo chown -R www-data:www-data /var/www/html/vmSafeguard # seulement l'utilisateur www:data et son grp aura des droits RWX, pour exec les scripts.php / ewmt.db
 sudo echo 'www-data ALL = (ALL) NOPASSWD: ALL' >> /etc/sudoers.d/myOverrides # voir pour mettre les droits à un dossier, et pas à "tout"
 sudo sed -i '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf # Permit to allow .htaccess/.htpasswd
 sudo a2enmod ssl
